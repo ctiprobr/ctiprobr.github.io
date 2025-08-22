6c9113a3c0198fb094a36f4ccaeff42213c69dd6
Flávio Casas de Arcega <flavio@arcega.com.br>
2025-08-22 00:16:46 -0300

    Melhorias de leiaute

--- conteudo/Artigos/2025-08-10_formacao-continuada.md
+++ conteudo/Artigos/2025-08-10_formacao-continuada.md
@@ -1,7 +1,7 @@
 Author: Editorial
 Title: Formação continuada
-Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab praesentium error pariatur magnam ex.
-Thumbnail: 2025-08-01_organizados.png
+Summary: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Thumbnail: 2025-08-10_formacao.png
 
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab praesentium error pariatur magnam ex. Expedita, quas dolores. Est, commodi facilis. Obcaecati velit cupiditate perferendis et dolorum illo perspiciatis, alias qui.
 
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,14 +6,12 @@ SITENAME = "CTI"
 SITETITLE = "Profissionais da Ciência e Tecnologia da Informação"
 SITESUBTITLE = "Espaço organizado de maneira independente com o objetivo de fomentar o debate sobre temas que orbitam a categoria profissional."
 
+PORT = 8080
+HTTPS = True
 SITEURL = "https://cti.pro.br"
 DOMAIN = SITEURL
 FEED_DOMAIN = SITEURL
-HTTPS = True
-
-PORT = 8080
-PATH = "conteudo"
-THEME = "theme"
+LINKS = ()
 
 TIMEZONE = "America/Sao_Paulo"
 DEFAULT_LANG = "pt-BR"
@@ -22,14 +20,16 @@ USE_FOLDER_AS_CATEGORY = True
 DISPLAY_PAGES_ON_MENU = True
 DISPLAY_CATEGORIES_ON_MENU = True
 
-# Blogroll
-LINKS = ()
-
-STATIC_PATHS = ["static"]
-PAGE_PATHS = ["Paginas"]
-
 DEFAULT_PAGINATION = 10
-SUMMARY_MAX_LENGTH = 200
-
-# Uncomment following line if you want document-relative URLs when developing
+SUMMARY_MAX_LENGTH = 100
+SUMMARY_MAX_PARAGRAPHS = 1
 RELATIVE_URLS = True
+
+THEME = "theme"
+PATH = "conteudo"
+PAGE_PATHS = ["Paginas"]
+STATIC_PATHS = ["static"]
+ARTICLE_URL = "{date:%Y}-{date:%m}-{date:%d}_{slug}/"
+ARTICLE_SAVE_AS = "{date:%Y}-{date:%m}-{date:%d}_{slug}/index.html"
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}/index.html"
